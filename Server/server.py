410e6b9e43be60f4be9de2999ef6ae3e3a58d8e5
Zonglin Li <lizonglin6565@163.com>
2017-05-28 11:52:22 -0500

    added commandline arguments

--- Server/server.py
+++ Server/server.py
@@ -3,44 +3,50 @@ import SocketServer
 import collections
 import random
 import threading
+import argparse
 
 class MyServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 
     class RequestHandler(SocketServer.BaseRequestHandler):
 
         def handle(self):
-            data = self.request.recv(1024).strip().split(',')
+
+            data = self.request.recv(1024).strip().split(",")
 
             if len(data) < 1:
                 return
 
             if len(data) == 1 and data[0] == "join":
-                id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(7))
+                id = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(7))
                 self.request.send(id)
-                print("new process joined with id %s" % id)
+                if self.server.verbose:
+                    print("new process joined with id %s" % id)
                 return
 
             if len(data) == 1 and data[0] == "act":
 
                 if len(self.server.t_queue) > 0 and self.server.t_set != None:
                     average = sum(self.server.t_queue) / float(len(self.server.t_queue))
-
-                    print("average = ", average, end=" ")
+                    if self.server.verbose:
+                        print("average = ", average, end=" ")
 
                     if average > (self.server.t_set + 1) and not self.server.on:
                         self.request.send("0") # 0: turn on
                         self.server.on = True
-                        print("turned on")
+                        if self.server.verbose:
+                            print("turned on")
                         return
 
                     if average < (self.server.t_set - 1) and self.server.on:
                         self.request.send("1") # 1: turn off
                         self.server.on = False
-                        print("turned off")
+                        if self.server.verbose:
+                            print("turned off")
                         return
 
                 self.request.send("2") # 2: hold
-                print("hold")
+                if self.server.verbose:
+                    print("hold")
                 return
 
             if len(data) == 2 and data[0] == "set":
@@ -48,7 +54,8 @@ class MyServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
                     self.server.t_set = float(data[1])
                 except Exception as e:
                     print(e)
-                print("set temperature to %f" % self.server.t_set)
+                if self.server.verbose:
+                    print("set temperature to %f" % self.server.t_set)
                 return
 
             if len(data) == 3:
@@ -59,21 +66,43 @@ class MyServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
                 id = data[0]
                 t = float(data[1])
                 h = float(data[2])
-                print("id = %s, t = %f, h = %f" % (id, t, h))
+                if self.server.verbose:
+                    print("id = %s, t = %f, h = %f" % (id, t, h))
                 self.server.t_queue.append(t)
                 self.server.h_queue.append(h)
 
 
-    def __init__(self, server_address, handler_class=RequestHandler, bind_and_activate=True, smooth_len=60):
+    def __init__(self, server_address, handler_class=RequestHandler, bind_and_activate=True, smooth_len=60, verbose=False):
         self.h_queue = collections.deque([], smooth_len)
         self.t_queue = collections.deque([], smooth_len)
         self.on = False
         self.t_set = None
         self.allow_reuse_address = True
+        self.verbose = verbose
         SocketServer.TCPServer.__init__(self, server_address, handler_class, bind_and_activate)
 
-if __name__ == '__main__':
-    server = MyServer(("192.168.1.103", 12290))
+def parse_cmd_args():
+    description = "Server for the SmartAC"  
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("--verbose", action="store_true",
+                        help="Set if you want to print the log on terminal")
+    parser.add_argument("-ip", type=str, default="0.0.0.0",
+                        help="ip address others can contact the server")
+    parser.add_argument("-port", type=int,
+                        help="The port number")
+    return parser
+
+
+
+if __name__ == "__main__":
+    parser = parse_cmd_args()
+    args = parser.parse_args()
+
+    if args.ip == None or args.port == None:
+        parser.print_help()
+        exit(2)
+
+    server = MyServer((args.ip, args.port), verbose=args.verbose)
     t = threading.Thread(target=server.serve_forever)
     t.setDaemon(True)
     t.start()
